edc164b6d3ce71b7aa4496298aff075eca7aaf78
Niko Vänttilä <niko.vanttila@gmail.com>
2024-02-26 14:43:52 +0200

    1.9: unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+          <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
