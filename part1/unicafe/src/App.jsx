7fe9dac6df9d8110d3e1ae3cc92d5de4e3b794ce
Niko Vänttilä <niko.vanttila@gmail.com>
2024-02-26 15:01:56 +0200

    1.10: unicafe step5 - PROGRESS1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.total === 0) {
     return (
@@ -12,53 +20,53 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.total}</p>
-        <p>average {(props.good - props.bad) / props.total}</p>
+        <StatisticLine text="good" value ={props.good} />
+        <StatisticLine text="neutral" value ={props.neutral} />
+        <StatisticLine text="bad" value ={props.bad} />
+        <StatisticLine text="all" value ={props.total} />
+        <StatisticLine text="average" value ={(props.good - props.bad) / props.total} />
+        <StatisticLine text="positive" value ={props.good / props.total} />
         <p>positive {props.good / props.total}%</p>
     </div>
   )
 }
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
-    // console.log("Good before click: ", good)
     const updatedGood = good + 1
     setGood(updatedGood)
     setTotal(updatedGood + neutral + bad)
-    // console.log("Good after click: ", good)
   }
 
   const handleNeutralClick = () => {
-    // console.log("Neutral before click: ", neutral)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
     setTotal(good + updatedNeutral + bad)
-    // console.log("Neutral after click: ", neutral)
   }
 
   const handleBadClick = () => {
-    // console.log("Bad before click: ", bad)
     const updatedBad = bad + 1
     setBad(updatedBad)
     setTotal(good + neutral + updatedBad)
-    // console.log("Bad after click: ", bad)
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-        <button onClick={handleGoodClick}>good</button>
-        <button onClick={handleNeutralClick}>neutral</button>
-        <button onClick={handleBadClick}>bad</button>
+        <Button handleClick={handleGoodClick} text="good" />
+        <Button handleClick={handleNeutralClick} text="neutral" />
+        <Button handleClick={handleBadClick} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
