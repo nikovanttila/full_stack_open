c70de2fb758cb132c1d0d6af2b05a1d6edf2298d
Niko Vänttilä <niko.vanttila@gmail.com>
2024-02-26 14:30:32 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,11 +5,13 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
     // console.log("Good before click: ", good)
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
     // console.log("Good after click: ", good)
   }
 
@@ -17,6 +19,7 @@ const App = () => {
     // console.log("Neutral before click: ", neutral)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
     // console.log("Neutral after click: ", neutral)
   }
 
@@ -24,6 +27,7 @@ const App = () => {
     // console.log("Bad before click: ", bad)
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
     // console.log("Bad after click: ", bad)
   }
 
@@ -37,6 +41,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {(good - bad) / total}</p>
+        <p>positive {good / total}%</p>
     </div>
   )
 }
