77db5cebae57c7a891ce3445da11a4b355a49d3f
Niko Vänttilä <niko.vanttila@gmail.com>
2024-03-04 13:32:17 +0200

    2.1: Course information step6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,29 @@
-const Course = (props) => {
+const Course = ({ course }) => {
     return (
-        <Header course={course} />
-        <Content
-          part1={part1}
-          exercises1={exercises1}
-          part2={part2}
-          exercises2={exercises2}
-          part3={part3}
-          exercises3={exercises3}
-        />
-        <Total sumOfExercises={exercises1 + exercises2 + exercises3} />
+        <div>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+        </div>
     )
 }
 
-const Header = (props) => {
-    return <h1>{props.course}</h1>
+const Header = ({ name }) => {
+    return <h1>{name}</h1>
 }
   
 const Total = (props) => {
     return <p>Number of exercises {props.sumOfExercises}</p>
 }
   
-const Part = (props) => {
+const Part = ({ name, exercises }) => {
     return (
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <p>{name} {exercises}</p>
     )
 }
   
-const Content = (props) => {
+const Content = ({ parts }) => {
     return (
-      <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.part3} exercises={props.exercises3} />
-      </div>
+      parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)
     )
 }
   
