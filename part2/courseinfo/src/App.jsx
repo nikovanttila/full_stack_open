9afc84e72293c1003bb48e24d80b38a03356167c
Niko Vänttilä <niko.vanttila@gmail.com>
2024-03-04 14:40:15 +0200

    2.4: Course information step9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,15 +1,15 @@
-const Course = ({ course }) => {
+const Course = ({ name, id, parts }) => {
     return (
         <div>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
+          <Header name={name} />
+          <Content parts={parts} />
+          <Total parts={parts} />
         </div>
     )
 }
 
 const Header = ({ name }) => {
-    return <h1>{name}</h1>
+    return <h2>{name}</h2>
 }
   
 const Total = ({ parts }) => {
@@ -32,9 +32,10 @@ const Content = ({ parts }) => {
 }
   
 const App = () => {
-    const course = {
-        id: 1,
+    const courses = [
+      {
         name: 'Half Stack application development',
+        id: 1,
         parts: [
           {
             name: 'Fundamentals of React',
@@ -50,11 +51,38 @@ const App = () => {
             name: 'State of a component',
             exercises: 14,
             id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
           }
         ]
       }
-    
-      return <Course course={course} />
+    ]
+
+    return (
+      <div>
+        <h1>Web development curriculum</h1>
+        {courses.map(course => <Course name={course.name} key={course.id} parts={course.parts} />)}
+      </div>
+    )
 }
   
 export default App
