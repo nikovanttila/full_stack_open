aee54a04b7a87b85f88a089f996ad7a0ddda26e9
Niko Vänttilä <niko@MacBook.local>
2024-04-16 13:53:52 +0300

    5.7 Blog List Frontend, step 7

--- part4/blog/bloglist-frontend/src/components/Blog.jsx
+++ part4/blog/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <div style={hideWhenVisible}>
+          {blog.title}
+          <button onClick={toggleVisibility}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+          {blog.title}
+          <button onClick={toggleVisibility}>cancel</button>
+          <div>
+            <div>{blog.url}</div>
+            <div>
+              likes {blog.likes}
+              <button onClick={null}>like</button>
+            </div>
+            <div>{blog.author}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const ErrorNotification = ({ message }) => {
   if (message === null) {
